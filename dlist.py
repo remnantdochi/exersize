347b8258a5f9d5698949b8baebe5455a658e4fb8
remnantdochi <remnantyeji@gmail.com>
2019-02-16 11:13:55 +0900

    make module dlist

--- dlist.py
+++ dlist.py
@@ -12,4 +12,20 @@ class dlist:
 
     def size(self): return self.size
 
-    
+    def insert_front(self,item):
+        if self.size == 0:
+            self.head=self.Node(None,item,None)
+            self.tail=self.head
+        else:
+            self.head=self.Node(None,item,self.head)
+        self.size+=1
+
+    def print_list(self):
+        p=self.head
+        while p:
+            print(p.item,end=" ")
+            p=p.next
+#sample=dlist()
+#sample.insert_front('apple')
+#sample.insert_front('orange')
+#sample.print_list()
