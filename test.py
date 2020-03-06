121150ee1393ca2956a01f163f05e78c5e30de2e
remnantdochi <remnantyeji@gmail.com>
2020-03-06 11:45:22 +0900

    11403_DFS

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 
-abc=list(map(int,input().split(' ')))
-print(abc)
-print({0}.format(abc[i] i for i in range(len(abc))))
+#abc=list(map(int,input().split(' ')))
+#print(abc)
+#print({0}.format(abc[i] i for i in range(len(abc))))
+
+
+sol=[[0,1,0],[0,0,0],[1,0,1]]
+
+sol[1] = [1 if sol[0][k] == 1 else 0 for k in range(3)]
+
+sol[2] = sol[0]+sol[1]+sol[2]
+print(sol[2])
