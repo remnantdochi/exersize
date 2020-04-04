847a1c0349672c9652573aeb55b78816796b05c2
remnantdochi <remnantyeji@gmail.com>
2020-04-04 21:56:34 +0900

    파이썬스러운가?

--- BJ1946.py
+++ BJ1946.py
@@ -7,11 +7,10 @@ for _ in range(T):
         nlist.append([s1,s2])
     nlist.sort(key=lambda element : element[0])
     ans = 0
+    minscore = N+1
     for i in range(N):
-        flag = False
-        for j in range(i):
-            if nlist[i][1] > nlist[j][1]:
-                flag = True
-                break
-        if flag != True: ans +=1
+        if minscore > nlist[i][1] :
+            ans +=1
+            minscore = nlist[i][1]
+
     print(ans)
